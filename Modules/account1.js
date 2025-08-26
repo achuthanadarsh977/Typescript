2a8acfdf0c34067c45b85e123b38ab5bac6c44b2
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-08-26 10:34:23 +0530

    Create account1.js

---
+++ Modules/account1.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Utils_1 = require("./Utils");
+var a = new Utils_1.ax('John Durairaj', 26, '1002', 'Mumbai', 'p1002');
+a.display();
