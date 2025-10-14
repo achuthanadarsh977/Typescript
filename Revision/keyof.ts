8ab0477c5e1cc2d1632ef4b625c1bfd31c81f8f6
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-10-14 18:05:15 +0530

    Create keyof.ts

---
+++ Revision/keyof.ts
@@ -0,0 +1,23 @@
+
+
+// type Indvidual = {
+//     name:string,
+//     age:number,
+//     id:number,
+//     address:string
+// }
+
+
+// type Indy = keyof Indvidual
+
+// let indy1 : Indy = "name"
+// let indy2 : Indy = "age"
+// let indy3 : Indy = "id"
+// let indy4 : Indy  = "address"
+
+// console.log(indy1,indy2,indy3,indy4)
+
+
+type Point = {x:number , y:number}
+
+type P = keyof Point
