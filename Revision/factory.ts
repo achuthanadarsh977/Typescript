b8f7b72522cd2a262bf182eb403cd26444dda9f7
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-10-15 13:16:03 +0530

    Create factory.ts

---
+++ Revision/factory.ts
@@ -0,0 +1,38 @@
+
+// function factory(name:string,age:number,address:string,gender:string){
+//     return{
+//         name,
+//         age,
+//         address,
+//         gender,
+//         greet:function(){
+//             console.log(`Hello my name is ${this.name} , I am ${this.age} years old , my address is ${this.address},
+//             I am a ${this.gender}`)
+//         }
+//     }
+// }
+
+
+// const sack = factory("Sam Thunder",23,"Chennai","Male")
+// sack.greet()
+
+
+// class Profile{
+//     name:string
+//     age:number
+//     gender:string
+//     constructor(name:string,age:number,gender:string){
+//         this.name = name
+//         this.age = age
+//         this.gender = gender
+//     }
+    
+//     display(){
+//         console.log(`Name:${this.name}`)
+//         console.log(`Age:${this.age}`)
+//         console.log(`Gender:${this.gender}`)
+//     }
+// }
+
+// const p = new Profile("John",23,"Male")
+// p.display()
