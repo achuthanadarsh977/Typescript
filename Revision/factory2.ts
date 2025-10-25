1c58e13418aba949e60dba98bf59a9feb6ba3380
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-10-25 10:48:25 +0530

    Create factory2.ts

---
+++ Revision/factory2.ts
@@ -0,0 +1,50 @@
+
+// function individual(name:string,age:number,id:number){
+//     return{
+//         name,
+//         age,
+//         id,
+//         greet(){
+//             console.log(`My name is ${this.name} , I am ${this.age} 
+//             years old, my id number is ${this.id}`)
+//         }
+//     }
+// }
+
+
+// const indy = individual('John Samuel',23,1001)
+
+// indy.greet()
+
+
+// function factory(name:string,age:number,id:number,address:string,gender:string){
+//     return{
+//         name,
+//         age,
+//         id,
+//         address,
+//         gender,
+//         myname(){
+//             console.log(`My name is ${this.name}`)
+//         },
+//         myaddress(){
+//           console.log(`My address is ${this.address}`)  
+//         },
+//         myid(){
+//             console.log(`My id is ${this.id}`)
+//         },
+//         mygender(){
+//             console.log(`I am ${this.gender}`)
+//         },
+//         myage(){
+//             console.log(`I am ${this.age} years old`)
+//         }
+//     }
+// }
+
+// const f = factory('Alex Ross',24,1001,'Chennai','Male')
+// f.myname()
+// f.myaddress()
+// f.myid()
+// f.mygender()
+// f.myage()
