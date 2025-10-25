ca4f60f03b2042fa44ec4035d2e7765b0a2e28c1
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-10-25 17:18:14 +0530

    Create revision.ts

---
+++ Revision/revision.ts
@@ -0,0 +1,54 @@
+// class Account{
+//     private balance:number
+//     constructor(balance:number){
+//         this.balance = balance
+//     }
+    
+//     deposit(amount:number){
+//         this.balance+=amount
+//     }
+    
+//     display(){
+//         console.log(`Total Balance:${this.balance}`)
+//     }
+    
+// }
+
+// const a = new Account(29000)
+// a.deposit(2000)
+// a.display()
+
+// abstract class Vehicle{
+    
+//     abstract startengine():void
+    
+//     stopengine():void{
+//         console.log('Engine has stopped')
+//     }
+// }
+
+// class Car extends Vehicle{
+    
+//     startengine():void{
+//         console.log('Engine has started')
+//     }
+// }
+
+// const v = new Car()
+// v.stopengine()
+// v.startengine()
+
+
+// interface Drawable{
+//     draw():void;
+// }
+
+// class Circle implements Drawable{
+//     draw():void{
+//         console.log('Drawable')
+//     }
+// }
+
+// const c = new Circle()
+// c.draw()
+
