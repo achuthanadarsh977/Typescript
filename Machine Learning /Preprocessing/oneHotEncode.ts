97772aefe3240c8f7d6f0e3bf9b872225980d3d7
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2026-02-14 10:27:42 +0530

    Update oneHotEncode.ts

--- Machine Learning /Preprocessing/oneHotEncode.ts
+++ Machine Learning /Preprocessing/oneHotEncode.ts
@@ -2,7 +2,11 @@
 
 
 
-
 function oneHotEncode(value:string , category:string[]):number[]{
-    return category.map()
+   return category.map(cat => (cat === value ? 1:0))
 }
+
+
+console.log(oneHotEncode("Red" , ["Red","Blue","Green"]))
+console.log(oneHotEncode("Blue" , ["Red","Blue","Green"]))
+console.log(oneHotEncode("Green" , ["Red","Blue","Green"]))
